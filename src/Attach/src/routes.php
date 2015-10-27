db74036963486911aebfd72e5a899d3b5b22d13a
Vitaly Nikolenko <vit@webstandart.ru>
2015-10-27 17:18:49 +0300

    -- tree remake

--- src/Attach/src/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::group(array('namespace' => '\LaravelAttach\Controller'), function() {
-    Route::get('download/{id}', array('as' => 'download_attach', 'uses' => 'AttachController@download'));
-});
