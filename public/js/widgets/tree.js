2fbce84ec9af26c4f71f85cfcf7ed6423687f2e3
Vitaly Nikolenko <vit@webstandart.ru>
2015-10-27 12:44:29 +0300

    -- tree remake

--- public/js/widgets/tree.js
+++ public/js/widgets/tree.js
@@ -6,16 +6,61 @@
         _create: function() {
 
             var $cont = this.element;
+
+
             $.getJSON( crud.format_setting("model_tree_url", {model: $cont.data('crud_tree'), scope: $cont.data('crud_scope')}), function (data) {
 
                     $cont.jstree({
-                        'core' : data,
-                        "state" : { "key" : "tree_"+$cont.data('crud_tree')+"_"+$cont.data('crud_scope') },
-                        "plugins" : [ "dnd", "contextmenu", "search", "state", "wholerow"  ] });
+                            'core': data,
+                            "state": {"key": "tree_" + $cont.data('crud_tree') + "_" + $cont.data('crud_scope')},
+                            "plugins": ["dnd", "contextmenu", "search", "state", "wholerow"],
+                            "contextmenu":{
+                                "items": function($node) {
+                                    var tree = $cont.jstree(true);
+                                    return {
+                                        "Create": {
+                                            "separator_before": false,
+                                            "separator_after": true,
+                                            "label": "Создать дочерний элемент",
+                                            "icon": "fa fa-plus-square",
+                                            "action": function (obj) {
+                                                console.log($node);
+
+                                            }
+                                        },
+                                        "Rename": {
+                                            "separator_before": false,
+                                            "separator_after": true,
+                                            "label": "Редактировать",
+                                            "icon": "fa fa-edit",
+                                            "action": function (obj) {
+
+                                            }
+                                        },
+                                        "Remove": {
+                                            "separator_before": false,
+                                            "separator_after": false,
+                                            "label": "Удалить",
+                                            "icon": "fa fa-trash",
+                                            "action": function (obj) {
+
+                                            }
+                                        }
+                                    };
+                                }
+                            }
+
+                        }
+                    );
 
                 }
             );
 
+        },
+        
+        refresh: function () {
+            //$('#mytree').jstree(true).settings.core.data = new_data;
+            //$('#mytree').jstree(true).refresh();
         }
 
     });
