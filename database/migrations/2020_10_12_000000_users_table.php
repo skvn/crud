8b8f5be8572743e8dd0ac96466d28339471ca93a
Vitaly Nikolenko <vit@webstandart.ru>
2016-01-07 15:28:08 +0300

    -- remove wizard

--- database/migrations/2020_10_12_000000_users_table.php
+++ database/migrations/2020_10_12_000000_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class ModUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('acl_role');
             $table->string('acl_abilities');
             $table->string('first_name');
