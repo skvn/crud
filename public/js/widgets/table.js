8d4f4d3a1cf5701aee891dd9e7845f8d8d805467
serg <ku1yamin_sergey@mail.ru>
2016-06-03 06:51:04 +0300

    -- locks

--- public/js/widgets/table.js
+++ public/js/widgets/table.js
@@ -81,7 +81,7 @@
                                     if (actions[i]['command'])
                                     {
                                         var com_url = crud.format_setting("model_command_url", {command:actions[i]['command'],id:rowData.id, model:tbl.data('crud_table'), scope:tbl.data('crud_scope')} );
-                                        buttons += '<li><a href="'+com_url+'" data-click="crud_action" data-action="crud_command" >';
+                                        buttons += '<li><a href="'+com_url+'" data-click="crud_action" data-action="crud_command" '+ (actions[i]['confirm'] ? 'data-confirm="'+actions[i]['confirm']+'"' : '') +'>';
                                     } else if (actions[i]['event'])
                                     {
                                         buttons += '<li><a href="#" data-model="'+tbl.data('crud_table')+'" data-scope="'+tbl.data('crud_scope')+'" data-id="'+rowData.id+'" data-click="crud_event" data-event="'+actions[i]['event']+'" >';
