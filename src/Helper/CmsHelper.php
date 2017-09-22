d61799c7a2da15e3380c7acc4a02b79f3e758c0e
serg <kulyamin_sergey@mail.ru>
2017-09-22 13:50:53 +0300

    transliteration moved to helper

--- src/Helper/CmsHelper.php
+++ src/Helper/CmsHelper.php
@@ -166,6 +166,36 @@ class CmsHelper
         return false;
     }
 
+    public function translitRussian($input, $url_escape = false, $tolower = false)
+    {
+        $arrRus = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м',
+                   'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь',
+                   'ы', 'ъ', 'э', 'ю', 'я',
+                   'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М',
+                   'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь',
+                   'Ы', 'Ъ', 'Э', 'Ю', 'Я', ];
+        $arrEng = ['a', 'b', 'v', 'g', 'd', 'e', 'jo', 'zh', 'z', 'i', 'y', 'k', 'l', 'm',
+                   'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'kh', 'c', 'ch', 'sh', 'sch', '',
+                   'y', '', 'e', 'yu', 'ya',
+                   'A', 'B', 'V', 'G', 'D', 'E', 'JO', 'ZH', 'Z', 'I', 'Y', 'K', 'L', 'M',
+                   'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'KH', 'C', 'CH', 'SH', 'SCH', '',
+                   'Y', '', 'E', 'YU', 'YA', ];
+
+        $input = str_replace(' ', '-', $input);
+        if ($tolower) {
+            $input = mb_strtolower($input, 'UTF-8');
+        }
+        $result = str_replace($arrRus, $arrEng, $input);
+        $result = preg_replace('#[^_-a-zA-Z0-9]#i', '', $result);
+        if ($url_escape) {
+            $result = str_replace([' ', '/', '\\'], '_', $result);
+            $result = urlencode($result);
+        }
+
+        return $result;
+    }
+    
+
 //
 
     public function getMonths()
--- src/Traits/ModelSlugTrait.php
+++ src/Traits/ModelSlugTrait.php
@@ -61,7 +61,7 @@ trait ModelSlugTrait
     {
         if (empty($slug)) {
             $slug = defined('static::SLUG_SOURCE') ? $this->getAttribute(static :: SLUG_SOURCE) : $this->getTitle();
-            $slug = $this->translitRussian($slug);
+            $slug = $this->app['skvn.cms']->translitRussian($slug);
             $slug = Str :: slug($slug);
         }
         $slug = $this->generateUniqueSlug($slug);
@@ -75,7 +75,7 @@ trait ModelSlugTrait
         $valid = $this->validateSlug($slug);
         if ($valid === -99) {
             if (defined('static::SLUG_FORCE_TRANSLIT')) {
-                $slug = $this->translitRussian($slug);
+                $slug = $this->app['skvn.cms']->translitRussian($slug);
                 $slug = Str :: slug($slug);
             } else {
                 throw new UniqueException('Slug('.$slug.') in model '.$this->classShortName.'#'.$this->getKey().' not in URI format. Acceptable format is [a-zA-Z0-9_-]');
@@ -89,27 +89,6 @@ trait ModelSlugTrait
             }
             throw new UniqueException('Slug column('.$slug.') for model '.$this->classShortName.'#'.$this->getKey().' is not unique');
         }
-//        $column = static :: slugColumn();
-//        if (! preg_match('#^[a-zA-Z0-9_-]+$#', $slug)) {
-//            if (defined('static::SLUG_FORCE_TRANSLIT')) {
-//                $slug = $this->translitRussian($slug);
-//                $slug = Str :: slug($slug);
-//            } else {
-//                throw new UniqueException($column.'('.$slug.') in model '.$this->classShortName.'#'.$this->getKey().' not in URI format. Acceptable format is [a-zA-Z0-9_-]');
-//            }
-//        }
-
-//        $id = $this->getKey() ? $this->getKey() : 0;
-//        $exists = $this->app['db']->table($this->table)->where($column, 'like', $slug)->where('id', '<>', $id)->get();
-//        if (count($exists) > 0) {
-//            if (defined('static::SLUG_GENERATE_NEXT')) {
-//                return $slug.'.'.(count($exists) + 1);
-//            }
-//            if (defined('static::SLUG_GENERATE_ID') && $this->exists) {
-//                return $slug.'.'.$this->getKey();
-//            }
-//            throw new UniqueException('Slug column('.$slug.') for model '.$this->classShortName.'#'.$this->getKey().' is not unique');
-//        }
 
         return $slug;
     }
@@ -128,34 +107,6 @@ trait ModelSlugTrait
         return 0;
     }
 
-    private function translitRussian($input, $url_escape = false, $tolower = false)
-    {
-        $arrRus = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м',
-            'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь',
-            'ы', 'ъ', 'э', 'ю', 'я',
-            'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М',
-            'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь',
-            'Ы', 'Ъ', 'Э', 'Ю', 'Я', ];
-        $arrEng = ['a', 'b', 'v', 'g', 'd', 'e', 'jo', 'zh', 'z', 'i', 'y', 'k', 'l', 'm',
-            'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'kh', 'c', 'ch', 'sh', 'sch', '',
-            'y', '', 'e', 'yu', 'ya',
-            'A', 'B', 'V', 'G', 'D', 'E', 'JO', 'ZH', 'Z', 'I', 'Y', 'K', 'L', 'M',
-            'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'KH', 'C', 'CH', 'SH', 'SCH', '',
-            'Y', '', 'E', 'YU', 'YA', ];
-
-        $input = str_replace(' ', '-', $input);
-        if ($tolower) {
-            $input = mb_strtolower($input, 'UTF-8');
-        }
-        $result = str_replace($arrRus, $arrEng, $input);
-        $result = preg_replace('#[^_-a-zA-Z0-9]#i', '', $result);
-        if ($url_escape) {
-            $result = str_replace([' ', '/', '\\'], '_', $result);
-            $result = urlencode($result);
-        }
-
-        return $result;
-    }
 
     public static function findBySlug($slug)
     {
@@ -168,7 +119,7 @@ trait ModelSlugTrait
     public function slugTransliterate($args = [])
     {
         if (! empty($args['source']) && ! empty($args[$args['source']])) {
-            $slug = $this->translitRussian($args[$args['source']]);
+            $slug = $this->app['skvn.cms']->translitRussian($args[$args['source']]);
             $slug = Str :: slug($slug);
 
             return $slug;
